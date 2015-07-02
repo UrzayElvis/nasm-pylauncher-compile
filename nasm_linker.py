77f80d7e6198c293a92e092b4123cdd27fec630d
Elvis Elvis C. Silfa <mrpride04@gmail.com>
2015-07-02 00:00:26 -0400

    Update nasm_linker.py

--- nasm_linker.py
+++ nasm_linker.py
@@ -16,15 +16,22 @@ gcc = []
 def catch():
 	print '\033[91m'+"E: linker couldn't perform the compilation and execution [Errors], Exiting..."
 	sys.exit(0)
-def run_file(file):
-	run = raw_input("Execute file (n/y) ")
-	if run=='n':
-		pass
-	else:
+def run_file(file,run_on_startup=False):
+	if run_on_startup:
 		subprocess.call("./"+file)
+		sys.exit(0)
+	else:
+		run = raw_input("Execute file (n/y) ")
+		if run=='n':
+			pass
+		else:
+			subprocess.call("./"+file)		
 def usage():
-	print "\nNSAM linker, made by https://github.com/UrzayElvis , for quick access compiling ASM poporses, you can redistribute this program freely, by Python programming \n\n"
-	print "Usage: ./nsam_linker.py [file]"
+	print "NSAM linker, made by https://github.com/UrzayElvis , for quick access compiling ASM poporses, you can redistribute this program freely, by Python programming\n"
+	print "-r   --run  :optional it'll look for an executable file, asm and will not compile just run the given asm file\n"
+	print "Examples: "
+	print "./nsam_linker.py [file] compile and run"
+	print "./nsam_linker.py [file] --run just run the file and do not compile"
 	sys.exit(0)
 def main():
 	global file
@@ -34,9 +41,10 @@ def main():
 	global gcc
 	if(len(sys.argv)<=1):
 		usage()
-
 	file = sys.argv[1]
 	classname = file.split('.')[0]
+	if(len(sys.argv[2]) and (sys.argv[2]=='--run' or sys.argv[2]=='-r')):
+		run_file(classname,True)
 	file_output_o = classname+'.o'
 	nasm = shlex.split("nasm -f elf "+file)
 	gcc = shlex.split('ld -m elf_i386 -s -o '+classname+' '+file_output_o)
